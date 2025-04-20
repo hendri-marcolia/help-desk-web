858b8ab9a94f95fa1ff620ae06c18818a0000845
Hendri Marcolia <hendri.marcolia@gmail.com>
2025-04-20 19:48:17 +0700

    Fix deployment due to the auto formatting problem

--- frontend/src/app/dashboard/[ticketId]/page.tsx
+++ frontend/src/app/dashboard/[ticketId]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const runtime = 'edge';
 import React from 'react';
 import { Reply } from '../../../api/models/Reply';
 import TokenHandler from './TokenHandler';
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
       <header className="flex items-center justify-between border-b bg-white px-8 py-4 shadow-sm">
         <Link href="/dashboard">
           <h1 className="flex items-center gap-2 text-2xl font-extrabold bg-gradient-to-r from-sky-500 to-indigo-500 bg-clip-text text-transparent cursor-pointer">
-            <span>💬</span> Facility's Help Desk
+            <span>💬</span> Facility&apos;s Help Desk
           </h1>
         </Link>
         <div className="flex items-center gap-4">
